8695260e8435175bb4111b61e100c3b80e9b1665
Lexlii <solodok81@gmail.com>
2021-11-01 16:22:49 +0000

    Fixing directory name

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from "./Screens/Login"
-import Home from "./Screens/Home"
+import Login from "./screens/Login"
+import Home from "./screens/Home"
 
 
 export default function App() {
