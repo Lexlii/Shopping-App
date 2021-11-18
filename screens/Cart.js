a1898a37054af3ccb087c79ffa51361bc49b89d4
Lexlii <solodok81@gmail.com>
2021-11-18 01:04:48 +0000

    Update

---
+++ screens/Cart.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+import { Ionicons, FontAwesome, AntDesign } from "@expo/vector-icons";
+
+export default function Cart(params) {
+  return (
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+        paddingTop: 55,
+        paddingHorizontal: 20,
+      }}
+    >
+    </View>
+  );
+}
